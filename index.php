c48de41ed4cba94b3f53201365e9234dee18c691
Mehmet Zaher Omer <mehmet-zaher.omer@outlook.de>
2024-04-20 12:53:04 +0200

    init

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>!.Versuch </title>
+</head>
+<body>
+   <h1> Meine erste seite </h1>
+   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis ea commodi sint praesentium modi cupiditate aut sed amet dicta tempore. Necessitatibus rem sit aliquam facilis accusamus facere fuga a veritatis delectus iste vero nostrum omnis, soluta provident tempora dignissimos laborum nam optio corporis consequuntur recusandae. Cumque ea non tempora provident, alias harum accusantium beatae consequuntur earum impedit explicabo laborum odit et eos iure itaque numquam aspernatur enim aliquid? Maiores, quidem suscipit dolores et voluptate minima saepe deserunt exercitationem, quia repellat ullam perspiciatis nam, praesentium magnam? Sed, unde facere labore nisi explicabo ratione perferendis ex dolores porro omnis laborum necessitatibus voluptatum incidunt rem enim soluta aliquam alias cupiditate, odio veritatis quisquam! Impedit quidem aliquam neque? Unde reprehenderit nostrum nisi consectetur aspernatur dolores, cumque impedit. Assumenda mollitia beatae maxime sunt harum recusandae non. Impedit quia quidem delectus aperiam. In impedit odio id corporis rem deleniti magni, nostrum ipsam quidem assumenda optio nam perspiciatis reprehenderit aspernatur obcaecati dolores quaerat similique iure ullam a vitae. Doloremque voluptas molestiae reprehenderit perspiciatis omnis ipsa laboriosam, expedita, labore sunt nam non ut asperiores facilis cumque harum quaerat nobis mollitia recusandae esse. Quo architecto corporis, quibusdam exercitationem vero vel distinctio earum tempore, omnis at magni, deleniti velit officiis?</p>
+    <p>
+        <?php
+        print("Hello World!");
+        echo "SO geht es auch...Ist aber veraltet";
+
+        phpinfo(); 
+        ?>
+    </p>
+</body>
+</html>
